e83d9220d455f8fab17a89e279ebca378a30d92d
Hana Maged <hannahmagedd27@example.com>
2024-07-17 23:19:41 +0300

    Project added

---
+++ Debug/FreeRTOS Source/list.d
@@ -0,0 +1,18 @@
+FreeRTOS Source/list.o: ../FreeRTOS\ Source/list.c \
+ /home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/FreeRTOS.h \
+ ../include/FreeRTOSConfig.h ../system/include/cmsis/system_stm32f4xx.h \
+ /home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/projdefs.h \
+ /home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/portable.h \
+ /home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/deprecated_definitions.h \
+ /home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/portable/GCC/ARM_CM4F/portmacro.h \
+ /home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/mpu_wrappers.h \
+ /home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/list.h
+/home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/FreeRTOS.h:
+../include/FreeRTOSConfig.h:
+../system/include/cmsis/system_stm32f4xx.h:
+/home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/projdefs.h:
+/home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/portable.h:
+/home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/deprecated_definitions.h:
+/home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/portable/GCC/ARM_CM4F/portmacro.h:
+/home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/mpu_wrappers.h:
+/home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/list.h:
---
+++ Debug/FreeRTOS Source/timers.d
@@ -0,0 +1,26 @@
+FreeRTOS Source/timers.o: ../FreeRTOS\ Source/timers.c \
+ /home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/FreeRTOS.h \
+ ../include/FreeRTOSConfig.h ../system/include/cmsis/system_stm32f4xx.h \
+ /home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/projdefs.h \
+ /home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/portable.h \
+ /home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/deprecated_definitions.h \
+ /home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/portable/GCC/ARM_CM4F/portmacro.h \
+ /home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/mpu_wrappers.h \
+ /home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/task.h \
+ /home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/list.h \
+ /home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/queue.h \
+ /home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/timers.h \
+ /home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/task.h
+/home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/FreeRTOS.h:
+../include/FreeRTOSConfig.h:
+../system/include/cmsis/system_stm32f4xx.h:
+/home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/projdefs.h:
+/home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/portable.h:
+/home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/deprecated_definitions.h:
+/home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/portable/GCC/ARM_CM4F/portmacro.h:
+/home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/mpu_wrappers.h:
+/home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/task.h:
+/home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/list.h:
+/home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/queue.h:
+/home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/timers.h:
+/home/helmenier/eclipse-workspace/Demo/FreeRTOS\ Source/include/task.h:
